84bf06c463af07e9555e0b5e69604143bc947dc1
shimpei ohtani <ohtani.shimpei@gmail.com>
2022-09-24 08:31:01 +0900

    フッター背景画像変更

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="bg-footer-texture h-footerHight relative w-full bg-center bg-cover">
+    <footer class="bg-footer-texture h-footerHight relative w-full bg-bottom bg-cover">
       <p class="text-center text-white absolute bottom-2 inset-x-0 text-shadow-md"><em>© 2021 Oki Meshi</em></p>
     </footer>
 </template>
