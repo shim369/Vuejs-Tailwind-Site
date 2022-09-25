fa78ce986ed55cfa4631a59bbcaac6f9639fa35c
shimpei ohtani <ohtani.shimpei@gmail.com>
2022-09-25 14:44:48 +0900

    font change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,7 +11,7 @@
         <a href=""></a>
       </div>
       <div class="flex items-center text-center h-full justify-center flex-col">
-      <h1 class="text-white text-6xl text-shadow-lg">Oki Meshi</h1>
+      <h1 class="text-white text-6xl text-shadow-lg mb-1">Oki Meshi</h1>
       <p class="text-white text-2xl text-shadow-lg">沖縄の飲食店に恩返しを</p>
       </div>
         <canvas class="h-waveHight -top-90 relative w-full align-top" id="sineCanvas" width="100%" height="90"></canvas>
