5173634c440a4b6477694532c54e68400d6ad35a
shimpei ohtani <ohtani.shimpei@gmail.com>
2022-09-25 15:52:08 +0900

    スライド修正

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@
       </div>
       <div class="flex items-center text-center h-full justify-center flex-col">
       <h1 class="text-white text-6xl text-shadow-lg mb-1">Oki Meshi</h1>
-      <p class="text-white text-2xl text-shadow-lg">沖縄の飲食店に恩返しを</p>
+      <p class="text-white text-2xl text-shadow-lg">沖縄の飲食店に恩返し</p>
       </div>
         <canvas class="h-waveHight -top-90 relative w-full align-top" id="sineCanvas" width="100%" height="90"></canvas>
     </header>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,11 +6,13 @@
         <div class="w-12/12 m-auto">
           <VueSlickCarousel v-bind="settings">
                   <div v-for="item in items" v-bind:key="item.index" class="card-item">
+                    <a :href="item.a" target="_blank">
                     <div class="card-inner border-solid border-2 border-gray-200">
                       <img :src="item.img" :alt="item.name"/>
                       <h3>{{item.name}}</h3>
                       <p>{{item.txt }}</p>
                     </div>
+                    </a>
                   </div>
           </VueSlickCarousel>
         </div>
@@ -21,19 +23,22 @@
 import VueSlickCarousel from 'vue-slick-carousel'
 import 'vue-slick-carousel/dist/vue-slick-carousel.css'
 import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+import 'vue-slick-carousel/src/slick-theme.css'
 export default {
   name: 'Main',
   components: { VueSlickCarousel },
     data() {
       return {
         settings: {
-          "autoplay": false,
+          "responsive": [],
+          "arrows": true,
+          "autoplay": true,
           "dots": true,
           "focusOnSelect": false,
           "infinite": true,
-          // "speed": 500,
-          "slidesToShow": 3,
-          "slidesToScroll": 3,
+          "speed": 1500,
+          "slidesToShow": 2,
+          "slidesToScroll": 2,
           // "touchThreshold": 5
         },
         items: [
@@ -41,16 +46,25 @@ export default {
             img: require('@/assets/images/naha/001/001.jpg'),
             name: '吉崎食堂 久茂地本店',
             txt: '沖縄家庭料理と元祖居酒屋メニューが楽しめます',
+            a: 'https://yura-net.com/',
           },
           { 
             img: require('@/assets/images/naha/002/001.jpg'),
             name: 'じまんや',
             txt: '自慢の料理と泡盛で最高の沖縄を堪能できます',
+            a: 'http://hpcc-j.com/',
           },
           { 
             img: require('@/assets/images/naha/003/001.jpg'),
             name: '乃魚島',
             txt: '鮮度抜群の魚介をさまざまな料理がおいしいです',
+            a: 'https://hitosara.com/0006127570/',
+          },
+          { 
+            img: require('@/assets/images/naha/004/001.png'),
+            name: '我那覇豚肉店',
+            txt: '高品質な沖縄県産のお肉を仕入れています',
+            a: 'https://www.ganaha-butaniku.co.jp/',
           },
         ],
       }
